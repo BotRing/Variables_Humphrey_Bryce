f7eacce14aa9877f733434c601e2b99a164e74c1
BotRing <bot.ring2007@gmail.com>
2025-02-26 10:07:56 -0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Variables_LastName_FirstName
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int favoriteNumber;
+            bool isJumping = false, isRunning = false;
+            float myFloat;
+            favoriteNumber = 6;
+            myFloat = 54.65f;
+            const double finalGrade = 90.0;
+
+            Console.WriteLine("Favorite #: " + favoriteNumber);
+            Console.WriteLine("Jumping: " + isJumping);
+            Console.WriteLine("Running: " + isRunning);
+            Console.WriteLine("Random Floating #: " + myFloat);
+            Console.WriteLine("Final Grade: " + finalGrade);
+        }
+    }
+}
