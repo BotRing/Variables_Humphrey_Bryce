7302cc6f0aed03568d5603141033d6f9fa5dd7bd
BotRing <bot.ring2007@gmail.com>
2025-02-26 10:20:30 -0600

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace Variables_LastName_FirstName
 {
@@ -6,13 +6,14 @@ namespace Variables_LastName_FirstName
     {
         static void Main(string[] args)
         {
-            int favoriteNumber;
-            bool isJumping = false, isRunning = false;
-            float myFloat;
-            favoriteNumber = 6;
-            myFloat = 54.65f;
-            const double finalGrade = 90.0;
+            int favoriteNumber; // Declares an interger to store a favorite number
+            bool isJumping = false, isRunning = false;  // Booleans initialized to false for jumping and running
+            float myFloat;  // Declared random float variable
+            favoriteNumber = 6; // Assigning the favoriteNumber to 6
+            myFloat = 54.65f;   // Assigning myFloat to 54.65
+            const double finalGrade = 90.0; // Initializing constant finalGrade to 90
 
+            // Prints all variables to console
             Console.WriteLine("Favorite #: " + favoriteNumber);
             Console.WriteLine("Jumping: " + isJumping);
             Console.WriteLine("Running: " + isRunning);
